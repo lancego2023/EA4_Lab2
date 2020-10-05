b86040fb797633f84fbcbca4211c0ee3baab9180
Robert Norwood <72119590+RobertNorwood2022@users.noreply.github.com>
2020-10-05 12:57:35 -0500

    Completed fixed.m, contains additional .dat file

--- fixed.m
+++ fixed.m
@@ -1,28 +1,48 @@
 clear
 clc
+global alpha %#ok<NUSED>
+global count  %#ok<NUSED>
 
-global alpha
-global count 
+%reads in data, orders computation and printing done
+[alpha, h, yinit, tfin] = textread('fixed.dat', '%f %f %f %f',1);
+fixed_solver(h,yinit,tfin);
 
+
+%actual computational work
+function [Tvector,Pvector] = fixed_solver(h,yinit,tfin)
+global count
 count = 0;
+%printing of names
 
-%change these
+%fprintf('Alpha: %f \n',alpha)
+fprintf('h: %f \n',h)
+fprintf('yinit: %f \n',yinit)
+fprintf('tfin: %f \n',tfin)
+
+%optional to change
 t0 = 0;
-tfin = 100;
-n = 100;
-p0 = 0.5;
-alpha = 0;
 
 %don't change these
-h = (tfin - t0)/n;
+p0 = yinit;
 P = p0;
 T = t0;
-for i = 1:n
+Pvector = [];
+Tvector = [];
+for i = 1:ceil((tfin-t0)/h)
+    if (T + h)>tfin
+        h = tfin - T;
+    end
     k1 = yprime(T,P);
     k2 = yprime(T+h,P+h*k1);
     k = (k1 + k2)/2;
+    Pvector = [Pvector; P];
+    Tvector = [Tvector; T];
     T = T + h;
     P = P + h*k;
 end
 
-fprintf('Final Result: %f',P)
+fprintf('Final Result for Y: %f \n',P)
+fprintf('Count: %i \n',count)
+end
+
+
